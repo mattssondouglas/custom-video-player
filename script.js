66195eba43f0709be53ba32d6e444116593d0dc2
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-09-19 14:39:48 +0200

    add functions, remove autoplay

--- script.js
+++ script.js
@@ -6,6 +6,37 @@ const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
 
+// Functions
+
+const toggleVideoStatus = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const updatePlayIcon = () => {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+};
+
+const updateProgress = () => {
+  console.log(video.currentTime);
+};
+
+const setVideoProgress = () => {
+  return true;
+};
+
+const stopVideo = () => {
+  video.currentTime = 0;
+  video.pause();
+};
+
 // Event listens
 
 video.addEventListener("click", toggleVideoStatus);
